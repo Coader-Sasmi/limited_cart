9b0f6f695f3e30cd0d677b1cf706a59aa2caae39
Coader-Sasmi <thesasmita.mahanta@gmail.com>
2025-04-16 18:45:35 +0530

    Logo Section done

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,9 +1,10 @@
-import { Hero } from "@/components";
+import { Hero, LogoSec } from "@/components";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <LogoSec />
     </>
   );
 }
---
+++ src/components/home/LogoSec.tsx
@@ -0,0 +1,32 @@
+import { Fragment } from "react";
+
+export default function LogoSec() {
+  const logoArr = [
+    {
+      img: "./logo_1.png",
+    },
+    {
+      img: "./logo_2.png",
+    },
+    {
+      img: "./logo_3.png",
+    },
+    {
+      img: "./logo_4.png",
+    },
+    {
+      img: "./logo_5.png",
+    },
+  ];
+  return (
+    <section className="shadow-xl">
+      <div className="main-container lg:p-16 md:p-8 p-6 grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 gap-8">
+        {logoArr.map((logo, i) => (
+          <Fragment key={i}>
+            <img src={logo.img} alt="logo" className="h-auto w-auto" />
+          </Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
