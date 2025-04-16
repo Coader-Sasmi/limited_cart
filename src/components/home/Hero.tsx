c3a2ff96acc06562bf979ae67c7ad325292f859d
Coader-Sasmi <thesasmita.mahanta@gmail.com>
2025-04-16 18:29:16 +0530

    Hero Section Done

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,3 +1,31 @@
+"use client";
+
 export default function Hero() {
-  return <section className="main-container py-10">Hero</section>;
+  return (
+    <section className="main-container py-10 ">
+      <div className="flex lg:flex-row flex-col gap-10 w-full">
+        <div className="w-full">
+          <img src="./hero_man.png" alt="image" className="h-auto w-auto" />
+        </div>
+        <div className="flex flex-col gap-5 items-center w-full ">
+          <div>
+            <img src="./girls.png" alt="image" className="h-auto w-auto" />
+          </div>
+          <div>
+            <img src="./hero_text.png" alt="image" className="h-auto w-auto" />
+          </div>
+          <p>NEW COLLECTION</p>
+          <button className="bg-black text-white w-40 py-2 text-sm rounded-md">
+            SHOP NOW
+          </button>
+          <div>
+            <img src="./hero_girl.png" alt="image" className="h-auto w-auto" />
+          </div>
+        </div>
+        <div className="w-full">
+          <img src="./hero_shoes.png" alt="image" className="h-auto w-auto" />
+        </div>
+      </div>
+    </section>
+  );
 }
