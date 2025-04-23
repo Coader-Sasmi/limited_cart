6e555430800ba713a2ca42d79a3403f29de5b529
Coader-Sasmi <thesasmita.mahanta@gmail.com>
2025-04-23 21:55:34 +0530

    Category Delete API integration

--- src/components/admin/CategoryCard.tsx
+++ src/components/admin/CategoryCard.tsx
@@ -1,15 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import { CustomDialog } from "@/components/core";
-import { CircularProgress } from "@mui/material";
+import { CircularProgress, Tooltip } from "@mui/material";
 import dayjs from "dayjs";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { AiFillEdit } from "react-icons/ai";
-import { MdOutlinePlaylistAdd } from "react-icons/md";
+import { MdOutlineDeleteOutline, MdOutlinePlaylistAdd } from "react-icons/md";
+import Swal from "sweetalert2";
 import { AddUpdateCategoryDialog } from ".";
+import useMutation from "../hooks/useMutation";
+import { sweetAlertCustomStyles } from "../utils";
+import errorHelper from "../utils/error";
 
 export default function CategoryCard({
   curCategory,
@@ -20,9 +25,37 @@ export default function CategoryCard({
 }) {
   const [openCategoryModel, setCategoryModel] = useState(false);
   const [removeLoading, setRemoveLoading] = useState(false);
+  const { mutation, isLoading } = useMutation();
   // const { push } = useRouter();
   // console.log(curCategory);
   const router = useRouter();
+
+  const deleteExpenses = async (id: string) => {
+    try {
+      Swal.fire({
+        title: "Warning..!",
+        text: "Are you sure you want to delete the Category?",
+        icon: "warning",
+        iconColor: "#FF4D49",
+        confirmButtonColor: "#FF4D49",
+        confirmButtonText: "Yes",
+        cancelButtonText: "No",
+        showCancelButton: true,
+        // customClass: sweetAlertCustomStyles,
+        backdrop: sweetAlertCustomStyles,
+      }).then(async (result) => {
+        if (result?.isConfirmed) {
+          const res = await mutation(`delete-category/${id}`, {
+            method: "DELETE",
+            isAlert: true,
+          });
+          res?.status === 201 && mutate && mutate();
+        }
+      });
+    } catch (error) {
+      errorHelper(error);
+    }
+  };
 
   return (
     <div className="admin-bg relative overflow-hidden p-5 flex flex-col gap-5 admin-card">
@@ -69,22 +102,35 @@ export default function CategoryCard({
           <p></p>
         </div>
         <div className="flex items-center justify-end gap-2 pt-3">
-          <div
-            onClick={() =>
-              router.push(
-                `/admin/category/subcategory?ID=${curCategory?.CategoryID}`
-              )
-            }
-            className="bg-tertiary cursor-pointer z-20 text-white h-8 w-8 flex items-center justify-center rounded-full"
-          >
-            <MdOutlinePlaylistAdd className="text-xl" />
-          </div>
-          <div
-            onClick={() => setCategoryModel(true)}
-            className="bg-primary cursor-pointer z-20 text-white h-8 w-8 flex items-center justify-center rounded-full"
-          >
-            <AiFillEdit className="text-xl" />
-          </div>
+          <Tooltip title="View">
+            <div
+              onClick={() =>
+                router.push(
+                  `/admin/category/subcategory?ID=${curCategory?.CategoryID}`
+                )
+              }
+              className="bg-green-600 cursor-pointer z-20 text-white h-8 w-8 flex items-center justify-center rounded-full"
+            >
+              <MdOutlinePlaylistAdd className="text-xl" />
+            </div>
+          </Tooltip>
+
+          <Tooltip title="Edit">
+            <div
+              onClick={() => setCategoryModel(true)}
+              className="bg-tertiary cursor-pointer z-20 text-white h-8 w-8 flex items-center justify-center rounded-full"
+            >
+              <AiFillEdit className="text-xl" />
+            </div>
+          </Tooltip>
+          <Tooltip title="Delete">
+            <div
+              className="z-20 w-8 h-8 flex items-center justify-center bg-primary text-white rounded-full cursor-pointer"
+              onClick={() => deleteExpenses(curCategory?.CategoryID)}
+            >
+              <MdOutlineDeleteOutline />
+            </div>
+          </Tooltip>
         </div>
       </div>
     </div>
