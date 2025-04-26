5d46e44b9fad2bcd67f68478f93c6083450dc1f2
Coader-Sasmi <thesasmita.mahanta@gmail.com>
2025-04-26 15:58:36 +0530

    Subcategory added

--- src/app/(website)/website/furniture/chairs/page.tsx
+++ src/app/(website)/website/furniture/chairs/page.tsx
@@ -1,7 +1,20 @@
-export default function page() {
+import { ProductCard } from "@/components";
+
+export default function FurniturePage() {
+  const productArrival = [
+    {
+      img: "/Picture1.png",
+      name: "Red Chair",
+    },
+  ];
+
   return (
-    <section className="main-container py-40 text-primary font-medium text-4xl uppercase flex justify-center ic">
-      COMING SOON
+    <section className="main-container py-10 flex flex-col justify-center items-center gap-8 ">
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 md:gap-10 gap-5 w-full ">
+        {productArrival?.map((item, i) => (
+          <ProductCard key={i} item={item} />
+        ))}
+      </div>
     </section>
   );
 }
--- src/app/(website)/website/furniture/sofas/page.tsx
+++ src/app/(website)/website/furniture/sofas/page.tsx
@@ -1,7 +1,48 @@
-export default function page() {
+import { ProductCard } from "@/components";
+
+export default function SofaPage() {
+  const productArrival = [
+    {
+      img: "/Picture2.png",
+      name: "White Sofa",
+    },
+    {
+      img: "/furniture/Picture10.png",
+      name: "Brown Sofa",
+    },
+    {
+      img: "/furniture/Picture11.png",
+      name: "Gray Sofa",
+    },
+    {
+      img: "/furniture/Picture12.png",
+      name: "Long White Sofa",
+    },
+    {
+      img: "/furniture/Picture13.png",
+      name: "Green Sofa",
+    },
+    {
+      img: "/furniture/Picture14.png",
+      name: "Dark Blue Sofa",
+    },
+    {
+      img: "/furniture/Picture15.png",
+      name: "Light Blue Sfa",
+    },
+    {
+      img: "/furniture/Picture16.png",
+      name: "Sofa",
+    },
+  ];
+
   return (
-    <section className="main-container py-40 text-primary font-medium text-4xl uppercase flex justify-center ic">
-      COMING SOON
+    <section className="main-container py-10 flex flex-col justify-center items-center gap-8 ">
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 md:gap-10 gap-5 w-full ">
+        {productArrival?.map((item, i) => (
+          <ProductCard key={i} item={item} />
+        ))}
+      </div>
     </section>
   );
 }
