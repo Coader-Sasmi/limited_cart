935693c5fda8542fcd3e8278e7ef94a9460391c4
Coader-Sasmi <thesasmita.mahanta@gmail.com>
2025-04-26 16:02:50 +0530

    Footwear Section done

--- src/app/(website)/website/footwear/boots/page.tsx
+++ src/app/(website)/website/footwear/boots/page.tsx
@@ -1,7 +1,36 @@
-export default function page() {
+import { ProductCard } from "@/components";
+
+export default function FootwearPage() {
+  const productArrival = [
+    {
+      img: "/product_1.png",
+      name: "Brown Boots",
+    },
+    {
+      img: "/product_2.png",
+      name: "Black Boots",
+    },
+    {
+      img: "/Picture3.png",
+      name: "Black Boots",
+    },
+    {
+      img: "/footwear/Picture5.png",
+      name: "Shoes",
+    },
+
+    {
+      img: "/footwear/Picture7.png",
+      name: "Brown Boots",
+    },
+  ];
   return (
-    <section className="main-container py-40 text-primary font-medium text-4xl uppercase flex justify-center ic">
-      COMING SOON
+    <section className="main-container py-10 flex flex-col justify-center items-center gap-8 ">
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 md:gap-10 gap-5 w-full ">
+        {productArrival?.map((item, i) => (
+          <ProductCard key={i} item={item} />
+        ))}
+      </div>
     </section>
   );
 }
--- src/app/(website)/website/footwear/sneakers/page.tsx
+++ src/app/(website)/website/footwear/sneakers/page.tsx
@@ -1,7 +1,19 @@
-export default function page() {
+import { ProductCard } from "@/components";
+
+export default function SnickersPage() {
+  const productArrival = [
+    {
+      img: "/footwear/Picture8.png",
+      name: "Snickers",
+    },
+  ];
   return (
-    <section className="main-container py-40 text-primary font-medium text-4xl uppercase flex justify-center ic">
-      COMING SOON
+    <section className="main-container py-10 flex flex-col justify-center items-center gap-8 ">
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 md:gap-10 gap-5 w-full ">
+        {productArrival?.map((item, i) => (
+          <ProductCard key={i} item={item} />
+        ))}
+      </div>
     </section>
   );
 }
