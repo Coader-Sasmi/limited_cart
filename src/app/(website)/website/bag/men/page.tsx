ad229f7072090a350f4ee4484e8aa41592409cdb
Coader-Sasmi <thesasmita.mahanta@gmail.com>
2025-04-29 21:42:23 +0530

    Bag added

--- src/app/(website)/website/bag/men/page.tsx
+++ src/app/(website)/website/bag/men/page.tsx
@@ -1,7 +1,21 @@
-export default function Page() {
+"use client";
+import { ProductCard } from "@/components";
+import { menBagProducts } from "@/components/utils/productCollections";
+import Link from "next/link";
+
+export default function BagPage() {
   return (
-    <section className="main-container py-40 text-primary font-medium text-4xl uppercase flex justify-center ic">
-      COMING SOON
+    <section className="main-container py-10 flex flex-col justify-center items-center gap-8 ">
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 md:gap-10 gap-5 w-full ">
+        {menBagProducts?.map((item, i) => (
+          <Link
+            href={`/website/product-details?category=menBag&id=${item?.id}`}
+            key={i}
+          >
+            <ProductCard item={item} />
+          </Link>
+        ))}
+      </div>
     </section>
   );
 }
--- src/app/(website)/website/bag/page.tsx
+++ src/app/(website)/website/bag/page.tsx
@@ -1,19 +1,21 @@
+"use client";
+import { ProductCard } from "@/components";
+import { bagProducts } from "@/components/utils/productCollections";
+import Link from "next/link";
+
 export default function BagPage() {
   return (
-    // <section className="main-container py-10 flex flex-col justify-center items-center gap-8 ">
-    //   <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 md:gap-10 gap-5 w-full ">
-    //     {bagProducts?.map((item, i) => (
-    //       <Link
-    //         href={`/website/product-details?category=bag&id=${item?.id}`}
-    //         key={i}
-    //       >
-    //         <ProductCard item={item} />
-    //       </Link>
-    //     ))}
-    //   </div>
-    // </section>
-    <section className="main-container py-40 text-primary font-medium text-4xl uppercase flex justify-center ic">
-      COMING SOON
+    <section className="main-container py-10 flex flex-col justify-center items-center gap-8 ">
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 md:gap-10 gap-5 w-full ">
+        {bagProducts?.map((item, i) => (
+          <Link
+            href={`/website/product-details?category=bag&id=${item?.id}`}
+            key={i}
+          >
+            <ProductCard item={item} />
+          </Link>
+        ))}
+      </div>
     </section>
   );
 }
